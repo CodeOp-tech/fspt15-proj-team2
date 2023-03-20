535f1becc72ec207340454c16e2012309d92ccfa
Crystal <crwainstock@gmail.com>
2023-03-20 19:34:43 +0100

    added notes/resources to vite.config file

--- my-express-app/client/vite-project/vite.config.js
+++ my-express-app/client/vite-project/vite.config.js
@@ -11,3 +11,9 @@ export default defineConfig({
     },
   },
 });
+
+// Both the front and back ends seem to be working on their own,
+// but I don't know if they're connected or not.
+// This post & repo helped me adjust this file.
+// Repo: https://github.com/Vanaldito/React-and-Express-boilerplate
+// Post: https://medium.com/@fredimanuelb/how-to-develop-a-react-and-express-application-using-vite-a493f3e844f5
