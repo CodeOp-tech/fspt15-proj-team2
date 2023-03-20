341511a87210180f313dcffd200ac999a047dea0
Crystal <crwainstock@gmail.com>
2023-03-20 15:51:19 +0100

    setupProxy middleware step -- front end still not rendering -- maybe error/missing in vite.config file

--- my-express-app/client/src/App.jsx
+++ my-express-app/client/src/App.jsx
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <div>
+        <h1>This is the front end.</h1>
+      </div>
     </div>
   );
 }
