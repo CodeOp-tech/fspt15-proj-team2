62ae879ecb5c2b71bae4740cdf4d7712942f33e6
Jacqueline Gellick <jgellick@gmail.com>
2023-03-27 19:59:36 -0400

    Added hero

--- my-express-app/client/vite-project/src/App.jsx
+++ my-express-app/client/vite-project/src/App.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
-import hero from "./pexels-cottonbro-studio-6686442.jpg"
-
 import "./App.css"; // This file is empty currently.
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <div>
         <Navbar/>
-        <img className="hero-img" src={hero} height="800px" width="1200px"/>
-        <h1>Find and enjoy the right podcast for you. </h1>
-        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio diam, tincidunt non tortor et, interdum gravida orci. Integer lectus felis, finibus a velit sed, pharetra sollicitudin libero. Sed vitae pulvinar ligula. </h6>
-        <div>Search bar</div>
+        <div className="hero-container">
+          <h1 className="hero-title">Podcast slogan. </h1>
+          <h4 className="hero-subtitle">Discover and enjoy podcasts.</h4>
+        </div>
+        <div>Search bar goes here</div>
+        <p>Favorites</p>
+        <p>Recommended</p>
       </div>
+      <Footer/>
     </div>
   );
 }
---
+++ my-express-app/client/vite-project/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div>
+            <p>Footer</p>
+        </div>  
+    )
+}
+
+export default Footer;
