bc6361f1151831e387e21d9a01b24b3f70a0f4d5
Jacqueline Gellick <jgellick@gmail.com>
2023-03-27 20:37:53 -0400

    Imported BrowserRouter and wrappped App in main.jsx file

--- my-express-app/client/vite-project/src/main.jsx
+++ my-express-app/client/vite-project/src/main.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // This file is empty currently.
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
