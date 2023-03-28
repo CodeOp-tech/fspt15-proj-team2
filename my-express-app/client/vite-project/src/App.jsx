38016c2a978fd7398139812f5f10a676e22ec414
Crystal <crwainstock@gmail.com>
2023-03-28 08:56:05 +0200

    basic search working -- returns 10 podcast episodes related to search

--- my-express-app/client/vite-project/src/App.jsx
+++ my-express-app/client/vite-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Search from "./Components/Search";
-import SearchResultsPage from "./Pages/SearchResultsPage";
 
 import "./App.css"; // This file is empty currently.
 
@@ -9,7 +8,7 @@ function App() {
     <div className="App">
       <div>
         <h1>This is the front end.</h1>
-        <SearchResultsPage />
+        <Search />
       </div>
     </div>
   );
--- my-express-app/client/vite-project/src/Components/Search.jsx
+++ my-express-app/client/vite-project/src/Components/Search.jsx
@@ -2,34 +2,35 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Search({ sendSearchTermCB }) {
+function Search() {
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
-  //   //Function for full search with API -- not working yet. getting 404 error
-  //   const searchPodcast = async (searchTerm) => {
-  //     setLoading(true);
-  //     let options = {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ searchTerm: searchTerm }),
-  //     };
-  //     try {
-  //       let results = await fetch(`/api/search`, options);
-  //       let data = await results.json();
-  //       console.log(data);
-  //       return data; // Returns array of 10 podcast episodes that meet search criteria
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //     setLoading(false);
-  //   };
+  //Function for full search with API -- not working yet. getting 404 error
+  const searchPodcast = async (searchTerm) => {
+    setLoading(true);
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ searchTerm: searchTerm }),
+    };
+    try {
+      let results = await fetch(`/api/search`, options);
+      let data = await results.json();
+      console.log(data);
+      return data; // Returns array of 10 podcast episodes that meet search criteria
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    sendSearchTermCB(searchTerm);
+    setSearchTerm(searchTerm);
+    searchPodcast(searchTerm);
     // useNavigate("/results"); //This page needs to be set up in the router area.
   };
 
