2476daa59c44a65d151d5ae31c2728dda89e9f1a
Jacqueline Gellick <jgellick@gmail.com>
2023-03-28 23:15:13 -0400

    Added search results page

---
+++ my-express-app/client/vite-project/src/Pages/SearchResultsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Components/Navbar.jsx";
+
+function SearchResultsPage() {
+    return (
+        <div className="search-results-page">
+            <Navbar/>
+            <h2>Search Results</h2>
+        </div>
+    )
+}
+
+export default SearchResultsPage;
--- my-express-app/client/vite-project/src/main.jsx
+++ my-express-app/client/vite-project/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ErrorPage from "./Pages/ErrorPage";
+import SearchResultsPage from "./Pages/SearchResultsPage";
 import "./index.css"; // This file is empty currently.
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     element: <App/>,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "search",
+    element: <SearchResultsPage/>,
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
