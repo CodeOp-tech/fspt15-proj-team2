08df4154371c2b8029dc46c7e9f808e3c03f657e
Jacqueline Gellick <jgellick@gmail.com>
2023-03-29 21:54:59 -0400

    Removed unneeded images

--- my-express-app/client/vite-project/src/Components/Player.jsx
+++ my-express-app/client/vite-project/src/Components/Player.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import play from "../Images/play.png";
-import rewind from "../Images/rewind.png";
-import fastforward from "../Images/fastforward.png";
 import "./Player.css";
 
 function Player() {
     return (
         <iframe src="https://www.listennotes.com/e/63405e9de44c49fda392918584c236c8/embed/" height="180px" width="100%"></iframe>
     )
-    // return (
-    //     <div className="player fixed-bottom">
-    //         <button className="rewind-btn"><img src={rewind}/></button>
-    //         <button className="play-btn"><img src={play}/></button>
-    //         <button className="pause-btn"><img src={fastforward}/></button>
-    //     </div>
-        
-    // )
 }
 
 export default Player;
