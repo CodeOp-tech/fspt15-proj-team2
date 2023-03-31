443388b85fe0dfecb523d52854fb0bab6e5de5a9
Crystal <crwainstock@gmail.com>
2023-03-31 10:18:28 +0200

    added search context provider in app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
+import { SearchContext } from "./SearchContext";
 import SearchResultsPage from "./Pages/SearchResultsPage";
 import PodcastDetailsPage from "./Pages/PodcastDetailsPage";
 import EpisodeDetailsPage from "./Pages/EpisodeDetailsPage";
 
 function App() {
-
   return (
     <Routes>
-      <Route  path="/" element={<LandingPage />}/>
-      <Route  path="/results" element={<SearchResultsPage />}/>
-      <Route  path="/podcast/:id" element={<PodcastDetailsPage />}/>
-      <Route  path="/episode/:id" element={<EpisodeDetailsPage />}/>
+      <SearchContext.Provider value={results}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/results" element={<SearchResultsPage />} />
+        <Route path="/podcast/:id" element={<PodcastDetailsPage />} />
+        <Route path="/episode/:id" element={<EpisodeDetailsPage />} />
+      </SearchContext.Provider>
     </Routes>
-)}
+  );
+}
 
 export default App;
