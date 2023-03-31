e108235f0d3ea00736305ec857470b12ebb6a6d7
Jacqueline Gellick <jgellick@gmail.com>
2023-03-31 04:25:05 -0400

    Added player component to episode details page

--- client/src/Pages/EpisodeDetailsPage.jsx
+++ client/src/Pages/EpisodeDetailsPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../Components/Navbar.jsx";
+import Player from "../Components/Player.jsx";
 
 function EpisodeDetails() {
     return (
         <div className="episode-details-container">
             <Navbar/>
             <h2>Episode Summary</h2>
+            <Player/>
         </div>
     )
 }
