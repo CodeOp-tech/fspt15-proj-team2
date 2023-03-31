e809ef3050a5867728c416221f3708ece7eae7e3
Crystal <crwainstock@gmail.com>
2023-03-31 11:47:52 +0200

    error saying results is undefined, so it's not rendering -- something happening in app file? not sure.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
 import SearchResultsPage from "./Pages/SearchResultsPage";
-import PodcastDetailsPage from "./Pages/PodcastDetailsPage"; //We don't need this in this iteration of the project.
+// import PodcastDetailsPage from "./Pages/PodcastDetailsPage"; //We don't need this in this iteration of the project.
 import EpisodeDetailsPage from "./Pages/EpisodeDetailsPage";
 import { SearchContext } from "./SearchContext";
 
 function App() {
-  const [results, setResults] = useState();
-
-  let searchResults = { results };
+  let { results } = useContext(SearchContext); // Getting error here saying results is undefined.
+  // Do we need this here? Without anything, the results value in the provider below was undefined. Not sure.
 
   return (
-    <SearchContext.Provider value={searchResults}>
+    <SearchContext.Provider value={results}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/results" element={<SearchResultsPage />} />
--- client/src/SearchContext.jsx
+++ client/src/SearchContext.jsx
@@ -1,10 +1,10 @@
 import { createContext } from "react";
 import { useState, useContext } from "react";
 
-export const SearchContext = createContext();
+export const SearchContext = createContext([]);
 
 function SearchContextProvider({ children }) {
-  const [results, setResults] = useState(null); // The data is searched in the Search component and then saved here.
+  const [results, setResults] = useState([]); // The data is searched in the Search component and then saved here.
 
   return (
     <SearchContext.Provider value={{ results, setResults }}>
