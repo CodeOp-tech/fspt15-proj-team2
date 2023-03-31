f4cf73cc4dace9c9b58eda387db1a64edb629a2d
Crystal <crwainstock@gmail.com>
2023-03-31 19:08:41 +0200

    added note about keeping search results in results component after going back from details page -- currently not working

--- client/src/Pages/EpisodeDetailsPage.jsx
+++ client/src/Pages/EpisodeDetailsPage.jsx
@@ -1,15 +1,24 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { SearchContext } from "../SearchContext";
 
 function EpisodeDetails() {
   const params = useParams(); //A part of react-router
   const ID = params.id; //Pulls the id from the react-router data to be used in the functions below --
   // this podcast episode id is in the database & can be used to search API
+  let { results, setResults } = useContext(SearchContext);
 
   return (
     <div className="episode-details-container">
       <Navbar />
+      {/* This link should keep the results from the previous search -- maybe refetch the data? 
+      Or store it locally? Or use onClick with this button to render it in the search results somehow...
+      not sure yet.*/}
+      <Link to="/results">
+        <button className="btn btn-primary mt-2">Back to search results</button>
+      </Link>
       <h2>Episode Summary</h2>
     </div>
   );
