7c5210e73ea6b30636e42de77ed7ebb62b137fc2
Jacqueline Gellick <jgellick@gmail.com>
2023-04-01 15:04:15 -0400

    Made Podcast App in navbar link back to landing page

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <footer className="page-footer font-small elegant-color-dark fixed-bottom">
             <div className="footer-copyright text-center p-3">
                 {/* attributes to flaticons */}
-                <a href="https://www.flaticon.com/free-icons/user" title="user icons"><p>User icon created by Freepik - Flaticon</p></a> <br/>
+                <a href="https://www.flaticon.com/free-icons/user" title="user icons"><p className="footer">User icon created by Freepik - Flaticon</p></a> <br/>
             </div>  
         </footer>
     )
--- client/src/Components/Player.jsx
+++ client/src/Components/Player.jsx
@@ -3,7 +3,7 @@ import "./Player.css";
 
 function Player() {
     return (
-        <div className="sticky-bottom">
+        <div className="fixed-bottom">
             <iframe  src="https://www.listennotes.com/e/63405e9de44c49fda392918584c236c8/embed/" height="180px" width="100%"></iframe>
         </div>
     )
