dee34ef36962260dbaf413ddd192253e0d3fc58f
Jacqueline Gellick <jgellick@gmail.com>
2023-04-04 16:06:09 -0400

    Changed navbar styling

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import avatar from "../Images/avatar.png";
+// import avatar from "../Images/avatar.png";
 
 function Navbar() {
   return (
@@ -9,10 +9,10 @@ function Navbar() {
       <Link to="/">
         <h6 className="nav-title">Podcast App</h6>
       </Link>
-      <div>
+      <div className="navbar-container">
         {/* if we prefer google material symbols we can switch it out with the flaticons */}
-        {/* <span className="material-symbols-outlined">account_circle</span> */}
-        <img src={avatar}></img>
+        <span className="avatar material-symbols-outlined">account_circle</span>
+        {/* <img src={avatar}></img> */}
         <button className="nav-login-btn btn">Login</button>
         <button className="nav-signup-btn btn">Sign up</button>
       </div>
--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -3,8 +3,9 @@ import React, { useState } from "react";
 import "../App.css";
 import Navbar from "../components/Navbar";
 // import Player from "../components/Player";
-import Footer from "../components/Footer";
 import Search from "../Components/Search";
+import hero from "../Images/hero.jpg"
+import hero2 from "../Images/hero2.jpg"
 // import Favorites from "../components/Favorites";
 // import Recommended from "../components/Recommended";
 
@@ -13,13 +14,10 @@ function LandingPage() {
     <div className="app">
       <Navbar />
       <div className="hero-container">
-        <h1 className="hero-title">Podcast App </h1>
+        <img className="hero-img" src={hero}/>
         <h4 className="hero-subtitle">Discover and enjoy podcasts</h4>
       </div>
-      {/* <Favorites/>
-        <Recommended/> */}
       <Search />
-      <Footer />
     </div>
   );
 }
