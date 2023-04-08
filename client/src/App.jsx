dd744ae2c29b80d516898f0acd726bb535ee33c5
lava-git <105036972+lava-git@users.noreply.github.com>
2023-04-08 21:28:53 +0200

    app.jsx changes with addition of routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,36 +5,21 @@ import LandingPage from "./Pages/LandingPage";
 import SearchResultsPage from "./Pages/SearchResultsPage";
 // import PodcastDetailsPage from "./Pages/PodcastDetailsPage"; //We don't need this in this iteration of the project.
 import EpisodeDetailsPage from "./Pages/EpisodeDetailsPage";
-<<<<<<< HEAD
-import Login from "./Pages/Login"
-import Register from "./Pages/Registration";
-=======
 import SearchContextProvider from "./SearchContext";
->>>>>>> main
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Routes>
-      <Route  path="/" element={<LandingPage />}/>
-      <Route  path="/results" element={<SearchResultsPage />}/>
-      <Route  path="/podcast/:id" element={<PodcastDetailsPage />}/>
-      <Route  path="/episode/:id" element={<EpisodeDetailsPage />}/>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-)}
-=======
     <SearchContextProvider>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/results" element={<SearchResultsPage />} />
         {/* <Route path="/podcast/:id" element={<PodcastDetailsPage />} /> */}
         <Route path="/episode/:id" element={<EpisodeDetailsPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </SearchContextProvider>
   );
 }
->>>>>>> main
 
 export default App;
