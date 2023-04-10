2d0da690937e91688984582220ff325f0f606cae
Crystal <crwainstock@gmail.com>
2023-04-10 15:04:24 +0200

    register and login functions working in postman

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const db = require("../model/helper");
+const jwt = require("jsonwebtoken");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -10,52 +14,55 @@ router.get("/", function (req, res, next) {
 async function userExists(req, res, next) {
   let { username } = req.body;
   let sql = `SELECT * FROM users WHERE username = "${username}"`;
-  let userFound = await db(sql)
-  if (userFound.data.length > 0) return res.status(400).send({message: "Username already exists!"});
+  let userFound = await db(sql);
+  if (userFound.data.length > 0)
+    return res.status(400).send({ message: "Username already exists!" });
   next();
 }
 
 // creating a new user
 router.post("/signup", userExists, async (req, res) => {
-  const { firstName, email, username, password  } = req.body;
-  const hashedPW = await bcrypt.hash(password, 10);
-
+  const { firstName, email, username, password } = req.body;
+  const stringPass = password.toString();
   try {
-      let sql = `INSERT INTO users (firstName, email, username, password) VALUES ("${firstName}", "${email}", "${username}", "${hashedPW}" )`
-      await db(sql);
-      res.status(200).send({message: "User registered!"});
-  } catch(err) {
-      // send the error in an object
-      res.status(400).send({error: err.message})
+    const hashedPW = await bcrypt.hash(stringPass, 10);
+    let sql = `INSERT INTO users (firstName, email, username, password) VALUES ("${firstName}", "${email}", "${username}", "${hashedPW}" )`;
+    await db(sql);
+    res.status(200).send({ message: "User registered!" });
+  } catch (err) {
+    // send the error in an object
+    res.status(400).send({ error: err.message });
   }
-  
-})
+});
 
 // log in for the user
-router.post("/login", userExists, async (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   // retrieve user from db
   try {
-      let sql = `SELECT * FROM users WHERE username="${username}";`
-      let result = await db(sql);
-      let user = result.data[0];
+    let sql = `SELECT * FROM users WHERE username="${username}";`;
+    let result = await db(sql);
+    let user = result.data[0];
 
-      // if user not found, return an error
-      if (!user) res.status(404).send({message: "User not found!"})
+    // if user not found, return an error
+    if (!user) res.status(404).send({ message: "User not found!" });
 
-      let doMatch = await bcrypt.compare(password, user.password);
-      if (doMatch) {
-          const token = jwt.sign({userID: user.id}, process.env.SUPER_SECRET)
-          res.send({message: "Log in successful! here is your token", token, username});
-      } else {
-          res.send({message: "The password is incorrect!"})
-      }
-      
-  } catch(err) {
-      // send the error in an object
-      res.status(400).send({message: err.message})
+    let doMatch = await bcrypt.compare(password, user.password);
+    if (doMatch) {
+      const token = jwt.sign({ userID: user.id }, process.env.SUPER_SECRET);
+      res.send({
+        message: "Log in successful! here is your token",
+        token,
+        username,
+      });
+    } else {
+      res.send({ message: "The password is incorrect!" });
+    }
+  } catch (err) {
+    // send the error in an object
+    res.status(400).send({ message: err.message });
   }
-})
+});
 
 module.exports = router;
