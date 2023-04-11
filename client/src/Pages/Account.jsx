2ea2bb66e00c40a79c81f45846c40117ee13405b
lava-git <105036972+lava-git@users.noreply.github.com>
2023-04-11 08:21:32 +0200

    account details page updates

--- client/src/Pages/Account.jsx
+++ client/src/Pages/Account.jsx
@@ -6,21 +6,25 @@ export default function Account() {
     return(
         <>
             <Navbar></Navbar>
-            <div className="d-flex flex-column justify-content-center align-content-center align-items-center m-4">
-                <h2>Account Details</h2>
+            <div className="d-flex flex-column justify-content-center align-content-center m-4">
+                <h2 style={{color: "#AA4A44"}}>Account Details</h2>
                 
                 
             </div>
 
             <div className="d-flex flex-column justify-content-center align-content-center align-items-left m-4">
-                <h3>Hello, user </h3>
-                <p>Account name:</p>
-                <p>Email:</p>
+                {/* these are things that we will eventually get from the database when the registration is working */}
+                <h3 className="mx-3">Hello, <span style={{color: "#AA4A44"}}>user_firstName</span></h3>
+                <div className="mx-5 mt-3">
+                    <p className="p-6"><b>username:</b> @sample</p>
+                    <p className="p-6"><b>email:</b> sample@gmail.com</p>
+                </div>
+                
             </div>
 
 
-            <div className="d-flex flex-column justify-content-center align-content-center align-items-center m-4">
-                <h3>Favorites</h3>
+            <div className="d-flex flex-column justify-content-center align-content-center  m-4">
+                <h3 style={{color: "#AA4A44"}} className="align-items-center">Favorites</h3>
                 <table class="table table-hover">
                     <thead>
                         <tr>
