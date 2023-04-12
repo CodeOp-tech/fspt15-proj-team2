088b0dafcea28610b995ee5a07cfd513a27611c5
Jacqueline Gellick <jgellick@gmail.com>
2023-04-11 22:05:10 -0400

    Added removeFromFavorites function on frontend

--- client/src/Pages/EpisodeDetailsPage.jsx
+++ client/src/Pages/EpisodeDetailsPage.jsx
@@ -66,9 +66,25 @@ function EpisodeDetails({setUrl, setShowPlayer}) {
     }
   }
 
-  // function removeFromFavorites() {
-
-  // }
+  async function removeFromFavorites() {
+    try {
+      let body = {
+        id: episodeData.id
+      };
+    let options = {
+      method: "DELETE",
+      headers: {"Content-Type": "application/json"}, 
+      body: JSON.stringify(body)
+      };
+    let results = await fetch("/users/favorites", options);
+    let data = await results.json();
+    // to change button
+    setChecked(false);
+    console.log(data);
+    } catch(err) {
+      console.log(err);
+    }
+  }
 
  return (
   <div className="episode-details-container">
@@ -99,7 +115,7 @@ function EpisodeDetails({setUrl, setShowPlayer}) {
             {episodeData.title_original && <div className="ep-info">
               {!hideListenButton && <button  className="fav-btn mb-3" onClick={() => playEpisode()}><span className="material-symbols-outlined down ">play_arrow</span><span> Listen </span></button>} <br/>
               {!checked && <button  className="fav-btn mb-2" onClick={() => addToFavorites()}><span className="material-symbols-outlined down">heart_plus</span><span> Add to favorites</span></button>}
-              { checked && <button  className="rem-btn mb-2"onClick={() => setChecked(false)}><span className="material-symbols-outlined down">favorite</span><span> Remove from favorites</span></button>} <br/>
+              { checked && <button  className="rem-btn mb-2"onClick={() => removeFromFavorites()}><span className="material-symbols-outlined down">favorite</span><span> Remove from favorites</span></button>} <br/>
               <span className="bold spaced-line release">Release Date: </span>
               <span>{dateObject?.toUTCString().slice(4, 16)}</span> <br/>
               <span className="bold spaced-line">Rating: </span>
