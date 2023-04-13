52dbd8f5ba4be84de9f1b843e88d81e68453139b
Crystal <crwainstock@gmail.com>
2023-04-13 10:51:33 +0200

    update App.jsx with isLoggedInRevert branch content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,3 @@
-import React, { useContext, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import LandingPage from "./Pages/LandingPage";
 import SearchResultsPage from "./Pages/SearchResultsPage";
 // import PodcastDetailsPage from "./Pages/PodcastDetailsPage"; //We don't need this in this iteration of the project.
 import EpisodeDetailsPage from "./Pages/EpisodeDetailsPage";
@@ -11,24 +7,37 @@ import Navbar from "./Components/Navbar";
 import Login from "./Pages/Login";
 import Registration from "./Pages/Registration";
 import Account from "./Pages/Account";
+import UserContext from "./UserContext";
+import useProvideAuth from "./hooks/useProvideAuth";
 
 function App() {
   const [url, setUrl] = useState("");
-  const [showPlayer, setShowPlayer] = useState(false)
+  const [showPlayer, setShowPlayer] = useState(false);
+  const auth = useProvideAuth();
 
   return (
-    <SearchContextProvider>
-      <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/results" element={<SearchResultsPage />} />
-        {/* <Route path="/podcast/:id" element={<PodcastDetailsPage />} /> */}
-        <Route path="/episode/:id" element={<EpisodeDetailsPage setUrl={setUrl} setShowPlayer={setShowPlayer}/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Registration />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-      {showPlayer && <Player url={url}/>}
-    </SearchContextProvider>
+    <UserContext.Provider value={auth}>
+      <SearchContextProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/results" element={<SearchResultsPage />} />
+          {/* <Route path="/podcast/:id" element={<PodcastDetailsPage />} /> */}
+          <Route
+            path="/episode/:id"
+            element={
+              <EpisodeDetailsPage
+                setUrl={setUrl}
+                setShowPlayer={setShowPlayer}
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Registration />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+        {showPlayer && <Player url={url} />}
+      </SearchContextProvider>
+    </UserContext.Provider>
   );
 }
 
