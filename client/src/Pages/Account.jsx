d558fc2230ec877a708a6093e1990a45dbd5b551
Crystal <crwainstock@gmail.com>
2023-04-13 10:41:48 +0200

    updating Account page for isLoggedInRevert

--- client/src/Pages/Account.jsx
+++ client/src/Pages/Account.jsx
@@ -3,27 +3,10 @@ import Navbar from "../Components/Navbar";
 import UserContext from "../UserContext";
 
 export default function Account() {
-  // No need to specify the method since GET is the default set
-  async function getUserInfo() {
-    let options = {
-      headers: {
-        authorization: "Bearer " + localStorage.getItem("token"),
-      },
-    };
-    try {
-      const { data } = await fetch("/users/login", options);
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    console.log(auth);
-  }, []);
+  const auth = useContext(UserContext);
 
   return (
-    <>
+    <div>
       <Navbar></Navbar>
       <div className="d-flex flex-column justify-content-center align-content-center m-4">
         <h2 style={{ color: "#AA4A44" }}>Account Details</h2>
@@ -32,14 +15,15 @@ export default function Account() {
       <div className="d-flex flex-column justify-content-center align-content-center align-items-left m-4">
         {/* these are things that we will eventually get from the database when the registration is working */}
         <h3 className="mx-3">
-          Hello, <span style={{ color: "#AA4A44" }}>user_firstName</span>
+          Hello,{" "}
+          <span style={{ color: "#AA4A44" }}>{auth.currentUser.firstName}</span>
         </h3>
         <div className="mx-5 mt-3">
           <p className="p-6">
-            <b>username:</b> @sample
+            <b>username: </b>@{auth.currentUser.username}
           </p>
           <p className="p-6">
-            <b>email:</b> sample@gmail.com
+            <b>email:</b> {auth.currentUser.email}
           </p>
         </div>
       </div>
@@ -79,6 +63,6 @@ export default function Account() {
           </tbody>
         </table>
       </div>
-    </>
+    </div>
   );
 }
