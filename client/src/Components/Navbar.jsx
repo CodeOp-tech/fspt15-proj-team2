f0bbc5abd3765ab1aae75cbed6dec9390e489b72
Crystal <crwainstock@gmail.com>
2023-04-13 10:43:00 +0200

    update navbar with content from isLoggedInRevert branch

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,9 +1,12 @@
-import {React} from "react";
+import { React, useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
+import UserContext from "../UserContext";
 
 function Navbar() {
+  const auth = useContext(UserContext);
+
   const reset = (e) => {
     setSearchTerm("");
   };
@@ -11,29 +14,46 @@ function Navbar() {
   const navigate = useNavigate();
 
   function login() {
-      navigate("/login")
+    navigate("/login");
   }
 
   function signup() {
-      navigate("/signup")
+    navigate("/signup");
   }
 
   function account() {
-    navigate("/account")
+    navigate("/account");
   }
 
   return (
-      <div className="navbar navbar-default navbar-fixed-top">
-          <Link to="/" onClick={reset}>
-            <h6 className="nav-title">Podcast App</h6>
-          </Link>
-          <div className="m-1 mr-2">
-              {/* if we prefer google material symbols we can switch it out with the flaticons */}
-              <span className="material-symbols-outlined">account_circle</span>
-              <a className="nav-login p-2" onClick={login}>Login</a>
-              <a className="nav-signup px-2" onClick={signup}>Sign up</a>
-          </div>
+    <div className="navbar navbar-default sticky-top">
+      <Link to="/" onClick={reset}>
+        <h6 className="nav-title">Podcast App</h6>
+      </Link>
+      <div className="m-1 mr-2 user-control">
+        {!auth.isLoggedIn && (
+          <>
+            <a className="nav-login p-2" onClick={login}>
+              Login
+            </a>
+            <a className="nav-signup px-2" onClick={signup}>
+              Sign up
+            </a>
+          </>
+        )}
+        {auth.isLoggedIn && (
+          <>
+            <span className="material-symbols-outlined" onClick={account}>
+              account_circle
+            </span>
+            <a className="nav-signup px-2" onClick={auth.logout}>
+              Logout
+            </a>
+          </>
+        )}
       </div>
-)};
+    </div>
+  );
+}
 
 export default Navbar;
